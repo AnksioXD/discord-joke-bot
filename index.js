e08e1e411ee4167aa7648720ff25bbd7cd8032cd
Anksio <62060135+AnksioXD@users.noreply.github.com>
2024-10-15 19:48:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,43 +4,33 @@ import 'dotenv/config'
 const TOKEN = process.env.TOKEN
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-function sendJokeEveryXMinutes(channelId, time) {
+function sendRoastEveryXMinutes(channelId, time) {
     setInterval(async () => {
-        const joke = await fetchJoke()
+        const roast = await fetch("https://evilinsult.com/generate_insult.php?lang=en&type=json")
+        const roastData = await roast.json()
 
         const channel = await client.channels.fetch(channelId);
-
-        if (channel) {
-            if (joke.type == "twopart") {
-                await channel.send(joke.setup);
-                setTimeout(async () => {
-                    await channel.send(joke.delivery);
-
-                }, 2000);
-            } else {
-                await channel.send(joke.joke);
-            }
-        } else {
-            console.log('Channel not found.');
-        }
+        await channel.send(roastData.insult)
     }, time);
 }
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-
-    const channelId = '1089214985013563434';
-    const time = 600000;
-
-    sendJokeEveryXMinutes(channelId, time)
-
-});
 
 async function fetchJoke() {
     const res = await fetch("https://v2.jokeapi.dev/joke/Programming,Dark")
     const data = await res.json()
     return data
 }
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Initiated RoastXmin`);
+
+    const channelId = '1089214985013563434';
+    const time = 600000;
+
+    sendRoastEveryXMinutes(channelId, time)
+
+});
+
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
