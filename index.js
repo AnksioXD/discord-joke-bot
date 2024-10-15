7d3d5c49469c577640f9a278245dbe140a92af2f
Anksio <62060135+AnksioXD@users.noreply.github.com>
2024-10-15 19:53:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ import { Client, GatewayIntentBits, REST, Routes } from 'discord.js';
 import 'dotenv/config'
 
 const TOKEN = process.env.TOKEN
+const CLIENT_ID = process.env.CLIENT_ID
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const commands = [
+    {
+        name: 'jokebro',
+        description: 'Replies with a Joke!',
+    },
+];
 
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+    console.error(error);
+}
 function sendRoastEveryXMinutes(channelId, time) {
     setInterval(async () => {
         const roast = await fetch("https://evilinsult.com/generate_insult.php?lang=en&type=json")
@@ -19,7 +37,6 @@ async function fetchJoke() {
     const data = await res.json()
     return data
 }
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`Initiated RoastXmin`);
